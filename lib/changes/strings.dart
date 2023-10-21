ac0403269e252ee881daa7319f1df35a4e66aec7
QasimALi <83086966+qasim1221@users.noreply.github.com>
2023-10-22 01:18:02 +0500

    Making Portfolio

---
+++ lib/changes/strings.dart
@@ -0,0 +1,30 @@
+String hellotag = '''Hi there, Welcome to My Space  ''';
+
+String yourname = "I'm Qasim Afzaal,";
+// String hometitle2 = "A Mobile Application\nDeveloper";
+
+String animationtxt1 = "A Mobile Application Developer";
+String animationtxt2 = "Flutter , Andriod, IOS";
+String animationtxt3 = "A Professional Software Engineer";
+
+//
+String contactHeadding = "Let’s try my service now!";
+String contactSubHeadding =
+    "Let’s work together and make everything super cute and super useful.";
+//
+String miniDescription =
+   " I'm an experienced Mobile Application Developer with expertise in Flutter, Android, IOS and React Native. I have over 3 years of experience and a strong in mobile app development, real-time communication, geolocation services, authentication methods, e-commerce solutions, payment gatway, push notification  and data-sharing applications";
+
+String servicesSubHeading =
+ """
+My skills include developing, maintaining mobile applications, crafting pixel-perfect UIs, adapting to new technologies, effective communication, architectural patterns (MVC, MVP, MVVM), mentoring, and working independently with a focus on well-architected projects.
+""";
+String protfolioSubHeading = """
+Throughout my journey as a designer and developer, I've been deeply involved in creating digital products that cater to both business and consumer needs. Notable projects include consultations for native mobile application development, real-time communication implementation using MQTT and WebRTC, Google Maps integration with location tracking capabilities, secure Number OTP and Biometric authentication solutions, contributions to e-commerce applications for enhanced shopping experiences, Websockets for real-time data exchange, and the development of a versatile data-sharing application, facilitating seamless sharing across devices. These projects collectively showcase my expertise in mobile app development, real-time communication, geolocation services, authentication methods, e-commerce solutions, and data-sharing applications.
+""";
+
+const String aboutMeHeadline =
+    'I\'m Sudesh Bandara, a Mobile App developer, Web developer and UI designer.';
+
+const String aboutMeDetail =
+    'I\'m a Fresh Graduate as Software Engineer from Quanrt University, UK. I have been developing mobile apps for over 4 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a CEO/Co-Founder of Onyxsio, Sri Lanka. Currently working as an Associate Software Engineer at Onyxsio.';
