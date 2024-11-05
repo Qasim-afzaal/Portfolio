28a324662ace7149c93957a5368bccd4c3014ccb
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-11-06 03:04:57 +0500

    push code

--- lib/changes/strings.dart
+++ lib/changes/strings.dart
@@ -3,9 +3,9 @@ String hellotag = '''Hi there, Welcome to My Space  ''';
 String yourname = "I'm Qasim Afzaal,";
 // String hometitle2 = "A Mobile Application\nDeveloper";
 
-String animationtxt1 = "A Mobile Application Developer";
-String animationtxt2 = "Flutter , Andriod, IOS";
-String animationtxt3 = "A Professional Software Engineer";
+String animationtxt1 = "Mobile Developer Expert";
+String animationtxt2 = "Flutter ,Andriod, IOS";
+String animationtxt3 = "Software Engineer over 6 year Experience";
 
 //
 String contactHeadding = "Let’s try my service now!";
@@ -13,18 +13,20 @@ String contactSubHeadding =
     "Let’s work together and make everything super cute and super useful.";
 //
 String miniDescription =
-   " I'm an experienced Mobile Application Developer with expertise in Flutter, Android, IOS and React Native. I have over 3 years of experience and a strong in mobile app development, real-time communication, geolocation services, authentication methods, e-commerce solutions, payment gatway, push notification  and data-sharing applications";
+   "With 6+ years in mobile development, I specialize in Flutter, Android, iOS, React Native, and OpenAI integrations. My expertise includes cross-platform apps, MVVM architecture, API integrations, CI/CD automation, and optimized performance. Skilled in database management (SQLite, Hive, Firebase) and Agile practices, I deliver seamless, high-quality user experiences.";
 
 String servicesSubHeading =
  """
-My skills include developing, maintaining mobile applications, crafting pixel-perfect UIs, adapting to new technologies, effective communication, architectural patterns (MVC, MVP, MVVM), mentoring, and working independently with a focus on well-architected projects.
+My skills encompass a wide range of mobile app development expertise, including creating and maintaining high-quality applications with pixel-perfect UIs, adapting seamlessly to emerging technologies, and implementing effective architectural patterns such as MVC, MVP, and MVVM. I have a strong focus on crafting well-architected projects, whether working independently or in a collaborative environment. My expertise also extends to AI integration with OpenAI, in-app purchases, in-app subscriptions, social authentication, app-to-app sharing, and web scraping. Additionally, I’m skilled in mentoring team members and ensuring efficient communication across all project stages
 """;
 String protfolioSubHeading = """
-Throughout my journey as a designer and developer, I've been deeply involved in creating digital products that cater to both business and consumer needs. Notable projects include consultations for native mobile application development, real-time communication implementation using MQTT and WebRTC, Google Maps integration with location tracking capabilities, secure Number OTP and Biometric authentication solutions, contributions to e-commerce applications for enhanced shopping experiences, Websockets for real-time data exchange, and the development of a versatile data-sharing application, facilitating seamless sharing across devices. These projects collectively showcase my expertise in mobile app development, real-time communication, geolocation services, authentication methods, e-commerce solutions, and data-sharing applications.
+Throughout my journey as a designer and developer, I've been extensively involved in creating over 15 products tailored to both business and consumer needs. My portfolio includes diverse projects, such as native mobile application development consultations, real-time communication using MQTT and WebRTC, and Google Maps integration for advanced location tracking. I’ve also implemented secure Number OTP and Biometric authentication, contributed to feature-rich e-commerce applications, and utilized WebSockets for seamless real-time data exchange.
+
+Notable projects cover a wide range of industries, including an AI-driven chat app with in-app subscription monetization, an AI-powered PDF generator, e-commerce platforms, web scraping solutions, and specialized applications in real estate, healthcare, and Uber-like location-based services. These projects collectively demonstrate my expertise in mobile app development, AI integration, real-time communication, geolocation services, various authentication methods, data-sharing applications, and robust e-commerce solutions.
 """;
 
 const String aboutMeHeadline =
     'I\'m Sudesh Bandara, a Mobile App developer, Web developer and UI designer.';
 
 const String aboutMeDetail =
-    'I\'m a Fresh Graduate as Software Engineer from Quanrt University, UK. I have been developing mobile apps for over 4 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a CEO/Co-Founder of Onyxsio, Sri Lanka. Currently working as an Associate Software Engineer at Onyxsio.';
+    'I\'m Graduate as Software Engineer from Fast University, UK. I have been developing mobile apps for over 6 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a CEO/Co-Founder of Onyxsio, Sri Lanka. Currently working as an Associate Software Engineer at Onyxsio.';
