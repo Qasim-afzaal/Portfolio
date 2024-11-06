7ff53721bc5aa614a9f851dbdd5694ae7f29899e
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-11-07 01:40:41 +0500

    add contact utils

--- lib/app/utils/contact_utils.dart
+++ lib/app/utils/contact_utils.dart
@@ -11,15 +11,15 @@ List<ContactUtils> contactUtils = [
   ContactUtils(
       url: gitHub,
       icon: "https://img.icons8.com/ios-glyphs/60/000000/github.png"),
-  ContactUtils(
-      url: fiverr,
-      icon: "https://img.icons8.com/ios-filled/50/000000/fiverr.png"),
-  ContactUtils(
-      url: facebook,
-      icon: "https://img.icons8.com/ios-filled/50/000000/facebook-new.png"),
-  ContactUtils(
-      url: youtube,
-      icon: "https://img.icons8.com/ios-filled/50/000000/youtube-play.png"),
+  // ContactUtils(
+  //     url: fiverr,
+  //     icon: "https://img.icons8.com/ios-filled/50/000000/fiverr.png"),
+  // ContactUtils(
+  //     url: facebook,
+  //     icon: "https://img.icons8.com/ios-filled/50/000000/facebook-new.png"),
+  // ContactUtils(
+  //     url: youtube,
+  //     icon: "https://img.icons8.com/ios-filled/50/000000/youtube-play.png"),
   ContactUtils(
       url: whatsapp,
       icon: "https://img.icons8.com/material-outlined/48/000000/whatsapp.png"),
@@ -29,9 +29,9 @@ List<ContactUtils> contactUtils = [
   ContactUtils(
       url: linkedin,
       icon: "https://img.icons8.com/ios-filled/50/000000/linkedin.png"),
-  ContactUtils(
-      url: upwork,
-      icon: "https://img.icons8.com/ios-filled/50/000000/upwork.png"),
+  // ContactUtils(
+  //     url: upwork,
+  //     icon: "https://img.icons8.com/ios-filled/50/000000/upwork.png"),
   ContactUtils(
       url: instagram,
       icon: "https://img.icons8.com/glyph-neue/64/000000/instagram-new.png"),
