6508fecfa113ff7377bc1f540e20f71f14e7b785
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-11-07 01:40:50 +0500

    add project utils

--- lib/app/utils/project_utils.dart
+++ lib/app/utils/project_utils.dart
@@ -14,17 +14,41 @@ class ProjectUtils {
 }
 
 List<ProjectUtils> projectUtils = [
+     ProjectUtils(
+    banners: 'assets/imgs/sparkd.jpeg',
+    icons: 'assets/imgs/sparkdIcon.png',
+    titles: 'SparkdAi',
+    description:
+        'SparkdAI elevates your dating and social interactions with AI-generated, personalized responses. Powered by advanced LLMs and OpenAIs API',
+    links: 'https://apps.apple.com/pk/app/sparkdai/id6504047689',
+  ),
+  ProjectUtils(
+    banners: 'assets/imgs/04.png',
+    icons: 'assets/imgs/converige.png',
+    titles: 'Convierge',
+    description:
+        'Real Estate App, revolutionizing property search and transactions. Our app offers intuitive property listings, interactive 3D tours, smart filters, and secure transactions for a seamless real estate experience',
+    links: 'https://convierge.ai/',
+  ),
   ProjectUtils(
     banners: 'assets/imgs/02.png',
-    icons: 'assets/imgs/flutter.png',
+    icons: 'assets/imgs/carryme.png',
     titles: 'Carry Me',
     description:
         'A practical solution to modern day world problems, CarryMe is a platform established to provide delivery services worldwide by connecting travelers – carriers, as we like to call – with people wanting to order from abroad. Customers can also register as verified carriers',
     links: 'https://pk.linkedin.com/company/carrymeaesier',
   ),
+    ProjectUtils(
+    banners: 'assets/imgs/bilmart.png',
+    icons: 'assets/imgs/bilmart_icon.png',
+    titles: 'Bilmart',
+    description:
+        'Extensive Product Catalog: Discover a vast selection of products across various categories. From clothing and accessories to electronics, gadgets, home decor, and beyond, find everything you need in one convenient app.',
+    links: 'https://pk.linkedin.com/company/carrymeaesier',
+  ),
   ProjectUtils(
     banners: 'assets/imgs/1.png',
-    icons: 'assets/imgs/flutter.png',
+    icons: 'assets/imgs/doctorprofile.png',
     titles: 'HealthTok',
     description:
         'It’s a doctor Patient app Doctor get Temperature, Heart Beat, Step count of Patient through smart watch and apple watch and also can chat & video call, book appointment included in this application.',
@@ -32,18 +56,11 @@ List<ProjectUtils> projectUtils = [
   ),
   ProjectUtils(
     banners: 'assets/imgs/03.png',
-    icons: 'assets/imgs/flutter.png',
+    icons: 'assets/imgs/the_gift_guide.png',
     titles: 'The GIft Guide',
     description:
         'focusing on web scraping, generic components, web views, and cross-platform UI for an enhanced gift discovery experience',
     links: 'https://thegiftguide.com/',
   ),
-  ProjectUtils(
-    banners: 'assets/imgs/04.png',
-    icons: 'assets/imgs/flutter.png',
-    titles: 'Convierge',
-    description:
-        'Real Estate App, revolutionizing property search and transactions. Our app offers intuitive property listings, interactive 3D tours, smart filters, and secure transactions for a seamless real estate experience',
-    links: '',
-  ),
+
 ];
